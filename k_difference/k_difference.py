259ee9d08c865390a755df2cfe580e79c17940f8
ryota sugimoto <ryota.sugimoto@gmail.com>
2012-07-13 14:33:51 +0900

    small optimize

--- k_difference/k_difference.py
+++ k_difference/k_difference.py
@@ -8,19 +8,6 @@ class K_Difference:
     self.Numbers = Numbers
 
   def __repr__(self):
-    commenout = '''def inner_search(nums, count = 0):
-      if nums:
-        target = self.K + nums[0]
-        index = bisect.bisect_left(nums[1:], target) + 1
-        try:
-          if nums[index] == target:
-            count = count + 1
-        except IndexError: pass
-        return inner_search(nums[1:],count)
-      else:
-        return count
-    '''
-    
     def inner_search(nums):
       offset = 0
       count = 0
